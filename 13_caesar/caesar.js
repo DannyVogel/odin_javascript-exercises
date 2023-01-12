f831fbc460a1edf65f3c07a8c8769c0f4e2b5ad8
DannyVogel <danny.hj.vogel@gmail.com>
2023-01-12 21:22:10 +0100

    update cipher to work with large negative shift factors; simplify shift calculation

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,35 +1,31 @@
 const caesar = function(str, num) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  
-  let strArr = str.split('')
-  
-  let encryptStrArr = []
-  
-  for(let i=0; i<strArr.length; i++){
-    if(strArr[i].match(/\W/g)){
-        encryptStrArr.push(strArr[i])
-        continue;
-    }
+    let strArr = str.split('')
+    let encryptStrArr = []
     
-    let ltrIndex = alphabet.indexOf(strArr[i].toLowerCase())
-    let newLtrIndex = 0
+    for(let i=0; i<strArr.length; i++){
+        if(strArr[i].match(/\W/g)){
+            encryptStrArr.push(strArr[i])
+            continue;
+        }
     
-    if(ltrIndex + num > 26){
-      newLtrIndex = (ltrIndex + num) % 26
-    } else if(ltrIndex + num < 0) {
-        newLtrIndex = ltrIndex + num + 26
-    } else if(ltrIndex + num == 26){
-        newLtrIndex = 0
-    } else {
-        newLtrIndex = ltrIndex + num
-    }
-    if(strArr[i] == strArr[i].toUpperCase()){
-      encryptStrArr.push(alphabet[newLtrIndex].toUpperCase())
-    } else {
-      encryptStrArr.push(alphabet[newLtrIndex])
-    }
-  }
-  return encryptStrArr.join("")
+        let ltrIndex = alphabet.indexOf(strArr[i].toLowerCase())
+        let newLtrIndex = 0
+        
+        if(ltrIndex + num >= 0){
+            newLtrIndex = (ltrIndex + num) % 26
+        } else {
+            newLtrIndex = ltrIndex + 26 - Math.abs(num % 26)
+        }
+
+        if(strArr[i] == strArr[i].toUpperCase()){
+            encryptStrArr.push(alphabet[newLtrIndex].toUpperCase())
+        } else {
+            encryptStrArr.push(alphabet[newLtrIndex])
+        }
+    } 
+
+    return encryptStrArr.join("")
 };
 
 // Do not edit below this line
